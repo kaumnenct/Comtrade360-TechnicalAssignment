bd0eb57b62f4a8442f376e5dc9e2657d929f9a47
kaumnen <main@komnen.xyz>
2022-04-04 17:24:57 +0200

    add /hello get endpoint which returns h1

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.get("/hello-rest", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
+});
+
+app.get("/hello", (req, res) => {
+  res.render("helloEndpoint", { message: "Hello World!" });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
