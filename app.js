5d7441b6b5aa24c3ce08c10a780f35fbfa6e29aa
kaumnen <main@komnen.xyz>
2022-04-05 15:27:24 +0200

    retrieving results from api or database,
    change origin of greetings by env variable, check it before sending a greeting towards frontend, add additional info about the origin on the frontend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import getLanguage from "./database/mongodb/getGreeting.js";
 import { auth } from "express-openid-connect";
 import addLanguage from "./database/mongodb/addNewLanguage.js";
+import translateGreeting from "./translateAPI/translate.js";
 
 //#region app config
 
@@ -28,44 +29,107 @@ app.set("view engine", "ejs");
 
 // #region Home Page
 app.get("/", (req, res) => {
+  let translation = process.env.TRANSLATION;
   let loggedInText = req.oidc.isAuthenticated()
     ? "You are logged in!"
     : "You are not logged in!";
-  res.render("index", { loggedInText: loggedInText });
+  res.render("index", {
+    loggedInText: loggedInText,
+    greetingOrigin: translation,
+  });
 });
 // #endregion
 
 // #region see all languages
 app.get("/hello-rest", (req, res) => {
-  let language = req.query.language;
-  if (language == "" || language == undefined) {
-    language = "en";
+  let translation = process.env.TRANSLATION;
+  if (translation == "api") {
+    let language = req.query.language;
+    if (language == "" || language == undefined) {
+      language = "en";
+    }
+    translateGreeting(language)
+      .then((result) => {
+        res
+          .status(200)
+          .json({
+            message: result.text,
+            info: "This greeting came from an api!",
+          });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error });
+      });
+  } else {
+    let language = req.query.language;
+    if (language == "" || language == undefined) {
+      language = "en";
+    }
+    getLanguage(language)
+      .then((result) => {
+        res
+          .status(200)
+          .json({
+            message: result.greeting,
+            info: "This greeting came from a MongoDB!",
+          });
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .json({ message: "Query error. Please check and try again!" });
+      });
   }
-  getLanguage(language)
-    .then((result) => {
-      res.status(200).json({ message: result.greeting });
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .json({ message: "Query error. Please check and try again!" });
-    });
 });
 
 app.get("/hello", (req, res) => {
-  let language = req.query.language;
-  if (language == "" || language == undefined) {
-    language = "en";
-  }
-  getLanguage(language)
-    .then((result) => {
-      res.render("helloEndpoint", { message: result.greeting });
-    })
-    .catch((error) => {
-      res.render("helloEndpoint", {
-        message: "Query error. Please check and try again!",
+  let translation = process.env.TRANSLATION;
+  if (translation == "api") {
+    let language = req.query.language;
+    if (language == "" || language == undefined) {
+      language = "en";
+    }
+    translateGreeting(language)
+      .then((result) => {
+        res.render("helloEndpoint", {
+          message:
+            result.text +
+            " - This greeting is retrieved from Google Translate API.",
+        });
+      })
+      .catch((error) => {
+        res.render("helloEndpoint", {
+          message: "Error. Please check and try again!",
+        });
       });
-    });
+  } else {
+    let language = req.query.language;
+    if (language == "" || language == undefined) {
+      language = "en";
+    }
+    getLanguage(language)
+      .then((result) => {
+        res.render("helloEndpoint", {
+          message:
+            result.greeting + " - This greeting is retrieved from MongoDB.",
+        });
+      })
+      .catch((error) => {
+        res.render("helloEndpoint", {
+          message: "Query error. No such language in our database!",
+        });
+      });
+  }
+});
+// #endregion
+
+// #region change greeting origin: database or API
+app.post("/greetingOrigin", (req, res) => {
+  let wayOfTranslation = req.body.translation;
+
+  //change way of translation through env variable
+  process.env["TRANSLATION"] = wayOfTranslation;
+  res.redirect("/");
 });
 // #endregion
 
